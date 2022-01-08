43660fc689b50a51a2b9af4e710a2ef9a65b35c3
joec1368 <chenforwork1368@gmail.com>
2022-01-09 02:34:22 +0800

    UI_bullet_done

---
+++ Assets/Script/bullet_Bar.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bullet_Bar : MonoBehaviour
+{
+    public Slider slider;
+    public Image fill;
+    public float bullet_now;
+    public Text text;
+    int temp = 0;
+
+    private void Start()
+    {
+        FillSlider();
+        bullet_now = slider.maxValue;
+        text.text = bullet_now.ToString();
+        
+    }
+    public void Update()
+    {
+        FillSlider();
+    }
+
+    public void FillSlider()
+    {
+        slider.value = bullet_now;
+        //slider.value = 10f;
+        text.text = bullet_now.ToString();
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            bullet_now--;
+            if(bullet_now == 0)
+            {
+                fill.enabled = false;
+                temp = 1;
+            }
+            else if(temp == 1)
+            {
+                temp = 0;
+                fill.enabled = true;
+                bullet_now = slider.maxValue;
+            }
+            else
+            {
+                temp = 0;
+                fill.enabled = true;
+            }
+        }
+    }
+}
