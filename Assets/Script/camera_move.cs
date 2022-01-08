e7edc38d4751e051b53e36f5ebce3f7d28789650
joec1368 <chenforwork1368@gmail.com>
2022-01-08 23:32:03 +0800

    UI_por

---
+++ Assets/Script/camera_move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0.3f, 0f, 0f);
+        if (transform.position.x <= 300)
+        {
+            transform.position = new Vector3(1145.347f, 199.5274f, 272.9381f);
+        }
+
+    }
+}
